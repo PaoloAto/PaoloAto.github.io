ea6be65ac2d32d93379acba3c421e29d08de4ad2
Bowdown4488 <paolo_ato@dlsu.edu.ph>
2022-05-26 19:30:31 +0800

    init

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+import { Section, SectionText, SectionTitle, SectionJob } from '../../styles/GlobalComponents';
+import Button from '../../styles/GlobalComponents/Button';
+import { LeftSection, Span } from './HeroStyles';
+import Typewriter from 'typewriter-effect'
+
+const Hero = (props) => (
+    // Accept 2 props
+    <Section row nopadding> 
+      <LeftSection>
+        <SectionTitle main center>
+          Hello, I'm <Span>Paolo</Span>. 
+        </SectionTitle> 
+        <SectionJob style={{ color: 'skyblue'}}>
+          Software Engineer 
+          {/* | <br></br> QA Engineer */}
+        </SectionJob>
+        <SectionText>
+          <Typewriter 
+            options={{
+              strings: [
+                'I am eager to learn new knowledge!',
+                'I love coding!',
+                'I like creating new things!'
+              ],
+              autoStart: true,
+              loop: true,
+              deleteSpeed: 10,
+              cursor: '<',
+              delay: 100,
+            }}
+            />
+        </SectionText>
+        {/* <Button onClick={props.handleClick}>Contact</Button> */}
+      </LeftSection>
+    </Section>
+);
+
+export default Hero;
